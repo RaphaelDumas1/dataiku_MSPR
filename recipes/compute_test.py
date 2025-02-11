31dfeefd70800ec5a4fb94d3ee66caf59501ce1e
admin <test@gmail.com>
2025-02-11 14:19:11 +0000

    Saved recipe: MSPR.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -27,17 +27,18 @@ count = 0
 for key, df in dfs.items():
     headers = df.columns 
     row = df.iloc[0]
+    col_count = 0
+    started = false
 
     for header, value in zip(headers, row):
-        col_count = 0
-        if(col_count > 0)
+        if(col_count > 0):
             col_count += 1
-        
-        if(header == "Sexe" or col_count == 1):
-            count += 1
+            
+        if(header == "Sexe" or started and col_count == 0):
             final_df.loc[count, "Année"] = key
-            final_df.loc[count, "Genre"] = value    
+            final_df.loc[count, "Genre"] = value   
             col_count = 1
+            started = true
             
         if(col_count == 2):
             final_df.loc[count, "Nom"] = value
@@ -49,7 +50,10 @@ for key, df in dfs.items():
             final_df.loc[count, "Voix"] = value
          
         if(col_count == 6)
-            col_count = 1
+            col_count = 0
+            count += 1
+            
+        
 
 # Write recipe outputs
 test = dataiku.Dataset("test")
