6721e098b08ee29ec2db543714139ef55a9846f4
admin <test@gmail.com>
2025-02-11 16:37:15 +0000

    Saved recipe: MSPR.compute_test1

--- recipes/compute_test1.py
+++ recipes/compute_test1.py
@@ -3,32 +3,72 @@ import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
-# Read recipe inputs
-Legislative_1997 = dataiku.Dataset("Legislative_1997")
-Legislative_1997_df = Legislative_1997.get_dataframe()
-Legislative_2007 = dataiku.Dataset("Legislative_2007")
-Legislative_2007_df = Legislative_2007.get_dataframe()
-Legislative_2017 = dataiku.Dataset("Legislative_2017")
-Legislative_2017_df = Legislative_2017.get_dataframe()
-Legislative_2002 = dataiku.Dataset("Legislative_2002")
-Legislative_2002_df = Legislative_2002.get_dataframe()
-Legislative_2024 = dataiku.Dataset("Legislative_2024")
-Legislative_2024_df = Legislative_2024.get_dataframe()
-Legislative_1993 = dataiku.Dataset("Legislative_1993")
-Legislative_1993_df = Legislative_1993.get_dataframe()
-Legislative_2012 = dataiku.Dataset("Legislative_2012")
-Legislative_2012_df = Legislative_2012.get_dataframe()
-Legislative_2022 = dataiku.Dataset("Legislative_2022")
-Legislative_2022_df = Legislative_2022.get_dataframe()
+legislative_2024 = dataiku.Dataset("Legislative_2024")
+legislative_2022 = dataiku.Dataset("Legislative_2022")
+legislative_2017 = dataiku.Dataset("Legislative_2017")
+legislative_2012 = dataiku.Dataset("Legislative_2012")
+legislative_2007 = dataiku.Dataset("Legislative_2007")
+legislative_2002 = dataiku.Dataset("Legislative_2002")
+legislative_1997 = dataiku.Dataset("Legislative_1997")
+legislative_1995 = dataiku.Dataset("Legislative_1995")
 
 
-# Compute recipe outputs
-# TODO: Write here your actual code that computes the outputs
-# NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
-test1_df = ... # Compute a Pandas dataframe to write into test1
+dfs = {
+    "2024" : legislative_2024.get_dataframe(),
+    "2022" : legislative_2022.get_dataframe(),
+    "2017" : legislative_2017.get_dataframe(),
+    "2012" : legislative_2012.get_dataframe(),
+    "2007" : legislative_2007.get_dataframe(),
+    "2002" : legislative_2002.get_dataframe(),
+    "1997" : legislative_1997.get_dataframe(),
+    "1995" : legislative_1995.get_dataframe(),
+}
 
+
+final_df = pd.DataFrame(columns=["Année", "Genre", "Nom", "Prénom", "Voix"])
+count = 0
+
+for key, df in dfs.items():
+    headers = df.columns 
+    row = df.iloc[0]
+    col_count = 0
+    started = False
+
+    for header, value in zip(headers, row):
+        if(header == "Blancs" or header == "Blancs et nuls"):
+            final_df.loc[count, "Année"] = key
+            final_df.loc[count, "Nom"] = "Blanc"
+            final_df.loc[count, "Prénom"] = "Blanc"
+            final_df.loc[count, "Voix"] = value
+            count += 1
+        
+        
+        if(col_count > 0):
+            col_count += 1
+            
+        if(header == "Sexe" or started and col_count == 0):
+            final_df.loc[count, "Année"] = key
+            final_df.loc[count, "Genre"] = value   
+            col_count = 1
+            started = True
+            
+        if(col_count == 2):
+            final_df.loc[count, "Nom"] = value
+            
+        if(col_count == 3):
+            final_df.loc[count, "Prénom"] = value
+
+        if(col_count == 4):
+            final_df.loc[count, "Voix"] = value
+         
+        if(col_count == 6):
+            col_count = 0
+            count += 1
+            
+        
 
 # Write recipe outputs
-test1 = dataiku.Dataset("test1")
-test1.write_with_schema(test1_df)
+# Dataset test renamed to Presidentielle by admin on 2025-02-11 15:22:10
+test = dataiku.Dataset("Presidentielle")
+test.write_with_schema(final_df)
