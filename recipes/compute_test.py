04d440d3f072951936208a35d773321370cfada3
admin <test@gmail.com>
2025-02-11 15:21:13 +0000

    Renamed dataset MSPR.MSPR__1__Presidentielle_2007 to MSPR.Presidentielle_2007

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -8,7 +8,8 @@ from dataiku import pandasutils as pdu
 presidentielle_2022 = dataiku.Dataset("Presidentielle_2022")
 presidentielle_2017 = dataiku.Dataset("MSPR__1__Presidentielle_2017")
 presidentielle_2012 = dataiku.Dataset("MSPR__1__Presidentielle_2012")
-presidentielle_2007 = dataiku.Dataset("MSPR__1__Presidentielle_2007")
+# Dataset MSPR__1__Presidentielle_2007 renamed to Presidentielle_2007 by admin on 2025-02-11 15:21:13
+presidentielle_2007 = dataiku.Dataset("Presidentielle_2007")
 # Dataset MSPR__1__Presidentielle_2002 renamed to Presidentielle_2002 by admin on 2025-02-11 15:21:03
 presidentielle_2002 = dataiku.Dataset("Presidentielle_2002")
 # Dataset MSPR__1__Presidentielle_1995 renamed to Presidentielle_1995 by admin on 2025-02-11 15:20:56
