039d8c6565bbe75fd65e2c03153fc167f416e89b
admin <test@gmail.com>
2025-04-13 12:11:01 +0000

    Saved recipe: MSPR.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -4,6 +4,4 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 client = dataiku.api_client()
-project = client.get_project("MSPR")
-
-plugin = project.get_plugin("excel-sheet-importer")
+plugin = client.get_plugin("excel-sheet-importer")
