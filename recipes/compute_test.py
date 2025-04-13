ab167a47b19c9a4a0157eadd56186f502cf8242f
admin <test@gmail.com>
2025-04-13 12:17:05 +0000

    Saved recipe: MSPR.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -5,14 +5,4 @@ from dataiku import pandasutils as pdu
 
 client = dataiku.api_client()
 plugin = client.get_plugin("excel-sheet-importer")
-desc = plugin.get_definition()
-
-# Parcourt les runnables
-if "runnables" in desc:
-    for runnable in desc["runnables"]:
-        print("ID          :", runnable.get("id"))
-        print("Label       :", runnable.get("label"))
-        print("Description :", runnable.get("description", ""))
-        print("-" * 30)
-else:
-    print("Ce plugin ne contient pas de runnables.")
+help(plugin)
