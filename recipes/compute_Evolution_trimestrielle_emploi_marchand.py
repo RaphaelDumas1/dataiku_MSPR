e96efdfe0ac6ca82f71132426af780bbb9f94d92
admin <test@gmail.com>
2025-04-14 08:41:10 +0000

    Saved recipe: MSPR.compute_Evolution_trimestrielle_emploi_marchand

--- recipes/compute_Evolution_trimestrielle_emploi_marchand.py
+++ recipes/compute_Evolution_trimestrielle_emploi_marchand.py
@@ -1,3 +1,38 @@
-from utils import run
+from utils import run, process
 
 run("MSPR", "aPmnwurD", "MSPR - Emploi.xlsx", [])
+
+datasets = [
+    {
+        "input": "Taux_de_chomage",
+        "output": "Taux_de_chomage",
+        "functions": []
+    },
+    {
+        "input": "Repartition_des_contrats",
+        "output": "Repartition_des_contrats",
+        "functions": []
+    },
+    {
+        "input": "Categorie_metiers",
+        "output": "Categorie_metiers",
+        "functions": [
+            {
+                 "name" : process_category_metier,
+                 "args" : []   
+            },
+            {
+                 "name" : pivot_years,
+                 "args" : []   
+            },
+            
+        ]
+    },
+    {
+        "input": "Nombre_de_salarie",
+        "output": "Nombre_de_salarie",
+        "functions": []
+    },
+]
+process(datasets)
+
