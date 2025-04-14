8677b548fff91f57cdae970a0e6aab01cc940351
admin <test@gmail.com>
2025-04-14 20:58:24 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -235,7 +235,7 @@ def process_pib(df):
         if i < len(df):
             val = df.at[i, first_col]
             if pd.notnull(val) and not str(val).startswith("dont "):
-                df.at[i, first_col] = "dont" + str(val)
+                df.at[i, first_col] = "dont" + str(val).strip()
 
     
     # 3. Supprimer lignes 1 à 4, 5, 8, 10, 14 et toutes les lignes après 19
