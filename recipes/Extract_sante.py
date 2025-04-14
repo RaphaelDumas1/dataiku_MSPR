50c9f32acb924a12fc927d405eae44589a5a6cb0
admin <test@gmail.com>
2025-04-14 19:13:44 +0000

    Saved recipe: MSPR.Extract_sante

--- recipes/Extract_sante.py
+++ recipes/Extract_sante.py
@@ -1,4 +1,4 @@
-from utils import create_datasets_from_file_sheets, delete_where_equal
+from utils import create_datasets_from_file_sheets, delete_where_equal, columns_to_int, columns_to_float
 
 datasets = [
     {
@@ -7,6 +7,16 @@ datasets = [
             {
                 "name" : delete_where_equal,
                 "args" : ["Espérance de vie sans incapacité femme", "nd"]
+            
+            },
+            {
+                "name" : columns_to_int,
+                "args" : [["Année"]]
+            
+            },
+            {
+                "name" : columns_to_float,
+                "args" : [["Espérance de vie femme"]]
             
             }
         ]
