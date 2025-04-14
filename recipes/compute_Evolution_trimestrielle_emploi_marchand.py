0d12d749c93086d8277c9087596fd1a9fb08be01
admin <test@gmail.com>
2025-04-14 09:22:57 +0000

    Saved recipe: MSPR.compute_Evolution_trimestrielle_emploi_marchand

--- recipes/compute_Evolution_trimestrielle_emploi_marchand.py
+++ recipes/compute_Evolution_trimestrielle_emploi_marchand.py
@@ -6,7 +6,7 @@ datasets = [
         "functions": [
             {
                 "name" : to_int,
-                "args" : ["Année"]
+                "args" : [["Année"]]
             }
         ]
     },
