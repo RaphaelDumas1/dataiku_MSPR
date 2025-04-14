4da391b92a6d2b065e712e799fef55aba3b7eea2
admin <test@gmail.com>
2025-04-14 14:33:20 +0000

    Saved recipe: MSPR.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -5,6 +5,10 @@ datasets = [
         "name": "Nombre_detranger",
         "functions": []
     },
+    {
+        "name": "Quotient_familiale",
+        "functions": []
+    },
 ]
 
 create_datasets_from_file_sheets("MSPR", "Datas", "MSPR - Demographie.xlsx", datasets, [])
