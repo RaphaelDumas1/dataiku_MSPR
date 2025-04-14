29a1f74cb9612afb7f88158e1b046a4bdd42fb8e
admin <test@gmail.com>
2025-04-14 12:48:50 +0000

    Saved recipe: MSPR.compute_Taux_scolarisation

--- recipes/compute_Taux_scolarisation.py
+++ recipes/compute_Taux_scolarisation.py
@@ -2,40 +2,7 @@ from utils import create_datasets_from_file_sheets, process, process_category_me
 
 datasets = [
     {
-        "name": "Taux_de_chomage",
-        "functions": [
-            {
-                "name" : to_int,
-                "args" : [["Année"]]
-            }
-        ]
-    },
-    {
-        "name": "Repartition_des_contrats",
-        "functions": [
-            
-        ]
-    },
-    {
-        "name": "Categorie_metiers",
-        "functions": [
-            {
-                 "name" : process_category_metier,
-                 "args" : []   
-            },
-            {
-                 "name" : pivot_years,
-                 "args" : []   
-            },
-            
-        ]
-    },
-    {
-        "name": "Nombre_de_salarie",
-        "functions": []
-    },
-    {
-        "name": "Evolution_trimestrielle_emploi",
+        "name": "MSPR_Taux_scolarisation",
         "functions": []
     },
 ]
