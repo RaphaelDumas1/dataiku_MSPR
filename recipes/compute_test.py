8d6010941072aff0f80193555a71b6402db18790
admin <test@gmail.com>
2025-04-14 14:31:23 +0000

    Saved recipe: MSPR.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -1,3 +1,14 @@
-from utils import run
+from utils import create_datasets_from_file_sheets
 
-run("MSPR", "aPmnwurD", "MSPR - Demographie.xlsx", ["Quotient familiale"])
+datasets = [
+    {
+        "name": "Taux_scolarisation",
+        "functions": []
+    },
+    {
+        "name": "annuaire_des_ecoles_en_france",
+        "functions": []
+    },
+]
+
+create_datasets_from_file_sheets("MSPR", "Datas", "MSPR - Demographie.xlsx", datasets, [])
