d8a23faabf1def4988342fef8cc0c38f3d3ffbb4
admin <test@gmail.com>
2025-04-14 13:28:26 +0000

    Saved recipe: MSPR.compute_Logement

--- recipes/compute_Logement.py
+++ recipes/compute_Logement.py
@@ -27,6 +27,19 @@ datasets = [
             }
         ]
     },
+    {
+        "name": "Categorie_logement",
+        "functions": [
+            {
+                 "name" : pivot_years,
+                 "args" : None
+            },
+            {
+                 "name" : column_from_float_to_string,
+                 "args" : ["Année"]
+            }
+        ]
+    },
 ]
 
 create_datasets_from_file_sheets("MSPR", "Datas", "MSPR - Emploi.xlsx", datasets, ["Categorie metiers"])
