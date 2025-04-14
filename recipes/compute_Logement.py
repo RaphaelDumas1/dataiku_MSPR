b42f1a56031a7876c4c48f669b9914a0c42d1479
admin <test@gmail.com>
2025-04-14 13:27:15 +0000

    Saved recipe: MSPR.compute_Logement

--- recipes/compute_Logement.py
+++ recipes/compute_Logement.py
@@ -1,3 +1,48 @@
-from utils import run
+from utils import create_datasets_from_file_sheets
 
-run("MSPR", "aPmnwurD", "MSPR - Logement.xlsx", [])
+datasets = [
+    {
+        "name": "Logement",
+        "functions": [
+            {
+                 "name" : pivot_years,
+                 "args" : None
+            },
+            {
+                 "name" : column_from_float_to_string,
+                 "args" : ["Année"]
+            }
+        ]
+    },
+    {
+        "name": "Repartition_des_contrats",
+        "functions": [
+            
+        ]
+    },
+    {
+        "name": "Categorie_metiers",
+        "functions": [
+            {
+                 "name" : process_category_metier,
+                 "args" : []   
+            },
+            {
+                 "name" : pivot_years,
+                 "args" : []   
+            },
+            
+        ]
+    },
+    {
+        "name": "Nombre_de_salarie",
+        "functions": []
+    },
+    {
+        "name": "Evolution_trimestrielle_emploi",
+        "functions": []
+    },
+]
+
+create_datasets_from_file_sheets("MSPR", "Datas", "MSPR - Emploi.xlsx", datasets, ["Categorie metiers"])
+
