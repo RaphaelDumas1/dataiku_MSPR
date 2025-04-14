efcdd2c490946cb2f8abaac21522191017aaba84
admin <test@gmail.com>
2025-04-14 09:17:13 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -121,13 +121,14 @@ def process_category_metier(df):
     
     return df
 
-def to_int(value):
-    if isinstance(value, str):
-        value = value.strip()
-    try:
-        return int(float(value))
-    except (ValueError, TypeError) as e:
-        raise ValueError(f"Impossible de convertir '{value}' en int.") from e
+def to_int(columns):
+    for column in columns:
+        if isinstance(value, str):
+            value = value.strip()
+        try:
+            return int(float(value))
+        except (ValueError, TypeError) as e:
+            raise ValueError(f"Impossible de convertir '{value}' en int.") from e
 
 def process(df, dataset):
     # Set variables for iteration
