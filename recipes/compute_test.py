ab7d218a4e8a5b5dc6fc18b09c9692d75f134d31
admin <test@gmail.com>
2025-04-14 14:32:37 +0000

    Saved recipe: MSPR.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -2,11 +2,7 @@ from utils import create_datasets_from_file_sheets
 
 datasets = [
     {
-        "name": "Taux_scolarisation",
-        "functions": []
-    },
-    {
-        "name": "annuaire_des_ecoles_en_france",
+        "name": "Nombre_detranger",
         "functions": []
     },
 ]
