1525733e8a1e4b296550ce5f06ab103e32d619fd
admin <test@gmail.com>
2025-04-14 07:40:04 +0000

    Saved recipe: MSPR.compute_Evolution_trimestrielle_emploi_marchand

--- recipes/compute_Evolution_trimestrielle_emploi_marchand.py
+++ recipes/compute_Evolution_trimestrielle_emploi_marchand.py
@@ -1,3 +1,3 @@
 from utils import run
 
-run("MSPR", "aPmnwurD", "MSPR - Emploi.xlsx", ["Evolution_trimestrielle_emploi"])
+run("MSPR", "aPmnwurD", "MSPR - Emploi.xlsx", ["Evolution trimestrielle emploi marchand"])
