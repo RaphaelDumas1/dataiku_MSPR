798d03ec0463b98d12ea4211213c91152fa896ac
admin <test@gmail.com>
2025-04-14 14:33:43 +0000

    Saved recipe: MSPR.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -9,6 +9,11 @@ datasets = [
         "name": "Quotient_familiale",
         "functions": []
     },
+    {
+        "input": "MSPR_Taux_de_natalite",
+        "output": "Taux_de_natalite",
+        "functions": []
+    },
 ]
 
 create_datasets_from_file_sheets("MSPR", "Datas", "MSPR - Demographie.xlsx", datasets, [])
