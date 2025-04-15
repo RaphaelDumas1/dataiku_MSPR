3d2052d4b5a9adc1f5085bc15d147ec62369ca6b
admin <test@gmail.com>
2025-04-15 06:22:12 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -224,6 +224,7 @@ def process_pib(df):
     if len(df) > 3:
         df.columns = df.iloc[2]
         df.columns = df.columns.str.replace(" \(r\)", "", regex=True)
+        df.columns = make_unique(df.columns)
     else:
         raise ValueError("Le DataFrame ne contient pas au moins 4 lignes pour définir les headers")
     
