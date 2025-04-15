fb674f06456f281a39ae94d34938c0948ca9eb37
admin <test@gmail.com>
2025-04-15 08:51:05 +0000

    Saved recipe: MSPR.Extract_education

--- recipes/Extract_education.py
+++ recipes/Extract_education.py
@@ -20,14 +20,15 @@ datasets = [
             {
                  "name" : columns_to_string,
                  "args" : [["code_postal", "code_commune", "code_departement", "code_academie", "code_region", "telephone",
-                           "siren_siret", "coordx_origine", "coordy_origine", "rpi_disperse", "code_type_contrat_prive", "code_bassin_formation"]]
+                           "siren_siret", "coordx_origine", "coordy_origine", "rpi_disperse", "code_type_contrat_prive", "code_bassin_formation",
+                           "code_nature]]
             },
             {
                  "name" : columns_to_int,
                  "args" : [["ecole_maternelle", "ecole_elementaire", "voie_generale", "voie_technologique", "voie_professionnelle",
-                           "restauration", "hebergement", "apprentissage", "segpa", "section_arts", "section_cinema", "section_theatre",
+                           "restauration", "hebergement", "ulis," "apprentissage", "segpa", "section_arts", "section_cinema", "section_theatre",
                            "section_sport", "section_internationale", "section_europeenne", "lycee_agricole", "lycee_militaire", "lycee_des_metiers",
-                           "post_bac", "greta", "nombre_d_eleves", "multi_uai", "rpi_concentre", "code_nature"]]
+                           "post_bac", "greta", "nombre_d_eleves", "multi_uai", "rpi_concentre"]]
             }
         ]
     },
