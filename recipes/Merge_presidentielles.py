9e9222139eb4e5b66aae95edb6aa4f080b6020bc
admin <test@gmail.com>
2025-04-15 12:14:22 +0000

    Saved recipe: MSPR.Merge_presidentielles

--- recipes/Merge_presidentielles.py
+++ recipes/Merge_presidentielles.py
@@ -93,6 +93,7 @@ for key, df in dfs.items():
             final_df.loc[count, "Nom"] = "Blanc"
             final_df.loc[count, "Prénom"] = "Blanc"
             final_df.loc[count, "Voix"] = value
+            final_df.loc[count, "Genre"] = "NA"
             count += 1
         
         # Increment since first candidate
