e718a28a41919c2efb8c128bca841f79e3dc25dc
admin <test@gmail.com>
2025-04-15 12:28:10 +0000

    Saved recipe: MSPR.compute_Legislative_2024

--- recipes/compute_Legislative_2024.py
+++ recipes/compute_Legislative_2024.py
@@ -1,3 +1,3 @@
 from utils import create_datasets_from_file_sheets
 
-create_datasets_from_file_sheets("MSPR", "Datas", "MSPR - Legislative.xlsx", [])
+create_datasets_from_file_sheets("MSPR", "Datas", "MSPR - Legislative.xlsx", [], [])
