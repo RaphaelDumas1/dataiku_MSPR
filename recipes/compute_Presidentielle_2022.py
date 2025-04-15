922dfb8fdd82624018f34cf3ad7d06ffb6917360
admin <test@gmail.com>
2025-04-15 12:32:57 +0000

    Saved recipe: MSPR.compute_Presidentielle_2022

--- recipes/compute_Presidentielle_2022.py
+++ recipes/compute_Presidentielle_2022.py
@@ -1,3 +1,38 @@
-from utils import run
+from utils import create_datasets_from_file_sheets
 
-run("MSPR", "aPmnwurD", "MSPR - Presidentielle.xlsx", [])
+datasets = [
+    {
+        "name": "Legislative_2024",
+        "functions": []
+    },
+    {
+        "name": "Legislative_2022",
+        "functions": []
+    },
+    {
+        "name": "Legislative_2017",
+        "functions": []
+    },
+    {
+        "name": "Legislative_2012",
+        "functions": []
+    },
+    {
+        "name": "Legislative_2007",
+        "functions": []
+    },
+    {
+        "name": "Legislative_2002",
+        "functions": []
+    },
+    {
+        "name": "Legislative_1997",
+        "functions": []
+    },
+    {
+        "name": "Legislative_1993",
+        "functions": []
+    },
+]
+
+create_datasets_from_file_sheets("MSPR", "Datas", "MSPR - Presidentielle.xlsx", datasets, [])
