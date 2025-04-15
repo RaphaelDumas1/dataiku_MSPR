66f7df87f2a29e708ece1a1d876055b7706179ac
admin <test@gmail.com>
2025-04-15 06:24:43 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -223,8 +223,7 @@ def rename_column(df, old_name, new_name):
 def process_pib(df):
     if len(df) > 3:
         print("Ligne 2 :", df.iloc[2].tolist())
-        df.columns = df.iloc[2]
-        df.columns = df.columns.str.replace(" \(r\)", "", regex=True)
+        df.columns = df.iloc[2].astype(str).str.strip().str.replace(" \(r\)", "", regex=True)
         df.columns = make_unique(df.columns)
     else:
         raise ValueError("Le DataFrame ne contient pas au moins 4 lignes pour définir les headers")
