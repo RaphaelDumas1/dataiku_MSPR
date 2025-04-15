146943ac177cd057cda8c91bf0953ed3101785f9
admin <test@gmail.com>
2025-04-15 07:56:17 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -311,7 +311,9 @@ def process_annuaire(df):
         
     for col, default in columns_with_defaults.items():
         df[col] = df[col].fillna("").apply(lambda x: x if str(x).strip() else default)
-        
+    
+    df["nombre_d_eleves"] = df["nombre_d_eleves"].fillna(df["nombre_d_eleves"].mean())
+    
     return df
     
     
