39383319549b0064978d3f91007e72467f4d88b1
admin <test@gmail.com>
2025-04-15 21:35:03 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -386,7 +386,7 @@ def execute_instruction_on_dataframe(df, instruction):
         # Set variables for iteration
         name = function["name"]
         args = function["args"] if function["args"] is not None else []
-
+        print("yes", df.columns)
         # Use function
         df = name(df, *args)
 
