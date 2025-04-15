2271fa64a302d735dc0f3348b6ec61bdf0466b62
admin <test@gmail.com>
2025-04-15 07:05:15 +0000

    Saved recipe: MSPR.Extract_economie

--- recipes/Extract_economie.py
+++ recipes/Extract_economie.py
@@ -16,13 +16,7 @@ datasets = [
                 "name" : columns_to_int,
                 "args" : [["Année"]]
             },
-            {
-                "name" : columns_to_float,
-                "args" : [[" Produit intérieur brut (PIB)", "Importations de biens et de services", "Dépense de consommation finale", "Dont ménages",
-                          "Dont administrations publiques", "Formation brute de capital fixe", "Dont sociétés et entreprises individuelles non financières",
-                          "Dont administrations publiques 2", "Dont ménages hors entrepreneurs individuels", "Exportations de biens et de services",
-                          "Demande intérieure hors stocks"], 1]
-            },
+            
         ]
     },
     {
