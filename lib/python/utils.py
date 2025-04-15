aa85910ddac36db9e35c291147c5384e6825019e
admin <test@gmail.com>
2025-04-15 11:26:58 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -406,6 +406,7 @@ def extract_and_concat_to_original(df, interval1, interval2):
     df_cleaned = df.drop(index=all_rows_to_drop).reset_index(drop=True)
     print("lll", df_cleaned.columns, df1.columns)
     
+    df_cleaned.rename(columns={'Age responsable dossier': 'Quotient familial'}, inplace=True)
     df1.rename(columns={'Age responsable dossier': 'Quotient familial'}, inplace=True)
     df2.rename(columns={'Situation familiale': 'Quotient familial'}, inplace=True)
 
