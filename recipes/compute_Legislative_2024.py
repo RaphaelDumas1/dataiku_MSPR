f6d6de5c41dbf785e37b4ef86531e764023fd934
admin <test@gmail.com>
2025-04-15 12:29:49 +0000

    Saved recipe: MSPR.compute_Legislative_2024

--- recipes/compute_Legislative_2024.py
+++ recipes/compute_Legislative_2024.py
@@ -1,3 +1,68 @@
 from utils import create_datasets_from_file_sheets
 
-create_datasets_from_file_sheets("MSPR", "Datas", "MSPR - Legislative.xlsx", [], [])
+datasets = [
+    {
+        "name": "Taux_de_chomage",
+        "functions": [
+            {
+                "name" : columns_to_int,
+                "args" : [["Année"]]
+            }
+        ]
+    },
+    {
+        "name": "Repartition_des_contrats",
+        "functions": [
+            {
+                "name" : columns_to_int,
+                "args" : []
+            }
+        ]
+    },
+    {
+        "name": "Categorie_metiers",
+        "functions": [
+            {
+                 "name" : process_category_metier,
+                 "args" : []   
+            },
+            {
+                 "name" : pivot,
+                 "args" : ["Année"]   
+            },
+            {
+                "name" : columns_to_int,
+                "args" : []
+            }
+            
+        ]
+    },
+    {
+        "name": "Nombre_de_salarie",
+        "functions": [
+            {
+                "name" : columns_to_int,
+                "args" : []
+            }
+        ]
+    },
+    {
+        "name": "Evolution_trimestrielle_emploi",
+        "functions": [
+            {
+                "name" : process_evolution_trimestrielle_emploi,
+                "args" : []
+            },
+            {
+                "name" : rename_column,
+                "args" : ["None", "Année"]
+            },
+            {
+                "name" : columns_to_int,
+                "args" : [["Année"]]
+            }
+        ]
+    },
+]
+
+create_datasets_from_file_sheets("MSPR", "Datas", "MSPR - Legislative.xlsx", datasets, [])
