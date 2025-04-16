e50caeee84d33d4e9409bf315adff7f4d1cab2ce
admin <test@gmail.com>
2025-04-16 09:55:03 +0000

    Saved recipe: MSPR.Extract_education

--- recipes/Extract_education.py
+++ recipes/Extract_education.py
@@ -20,7 +20,10 @@ datasets = [
     {
         "name": "annuaire_des_ecoles_en_france",
         "functions": [
-           
+            {
+                "name" : delete_where_not_equal,
+                "args" : ["code_departement", 35.0]
+            },
             {
                 "name" : delete_columns_by_name,
                 "args" : [['code_departement', 'code_region', 'libelle_departement', 'libelle_region']]
