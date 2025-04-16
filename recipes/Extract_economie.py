b3a142964f3bb4024e348977cbe2af8e6a5fedf6
admin <test@gmail.com>
2025-04-16 13:16:12 +0000

    Saved recipe: MSPR.Extract_economie

--- recipes/Extract_economie.py
+++ recipes/Extract_economie.py
@@ -9,7 +9,7 @@ datasets = [
         "functions": [
             {
                 "name" : set_row_as_headers,
-                "args" : []
+                "args" : [1, pib_column]
             },
             {
                 "name" : process_pib,
