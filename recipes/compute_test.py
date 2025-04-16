f495a84a2bee3050815c778a504a934a5a576900
admin <test@gmail.com>
2025-04-16 08:02:41 +0000

    Saved recipe: MSPR.compute_test

--- recipes/compute_test.py
+++ recipes/compute_test.py
@@ -1,4 +1,7 @@
-import dataiku
+recipe = project.get_recipe(recipe_name)
+
+# Récupérer les datasets d'entrée de la recette
+input_datasets = recipe.list_input_names()import dataiku
 import pandas as pd
 from sqlalchemy import create_engine
 
@@ -7,9 +10,12 @@ engine = create_engine('postgresql://postgres:test@localhost:5432/MSPR')
 
 # Liste des datasets du projet
 project = dataiku.api_client().get_project("MSPR")
-datasets = project.list_datasets()
+datasets = project.list_datasets()recipe = project.get_recipe("compute_test")
 
-for ds in datasets:
+# Récupérer les datasets d'entrée de la recette
+input_datasets = recipe.list_input_names()
+
+for ds in input_datasets:
     dataset_name = ds["name"]
     dku_dataset = dataiku.Dataset(dataset_name)
     
