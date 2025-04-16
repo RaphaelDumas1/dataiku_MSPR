18210f8e083053db4d84e2bd81ff8036960a7693
admin <test@gmail.com>
2025-04-16 13:23:28 +0000

    Saved recipe: MSPR.Extract_economie

--- recipes/Extract_economie.py
+++ recipes/Extract_economie.py
@@ -12,14 +12,28 @@ datasets = [
                 "args" : [1, pib_column]
             },
             {
+                "name" : process_pib,
+                "args" : []
+            },
+            {
                 "name" : delete_rows_by_index,
                 "args" : [[0, 1, 2, 6, 8, 12], 17]
             },
             {
-                "name" : process_pib,
-                "args" : []
+                "name" : pivot,
+                "args" : ["Année"]
             },
-            
+            {
+                "name" : columns_to_int,
+                "args" : [["Année"]]
+            },
+            {
+                "name" : columns_to_float,
+                "args" : [["Produit intérieur brut (PIB)", "Importations de biens et de services", "Dépense de consommation finale", "Dépense de consommation finale dont ménages",
+                          "Dépense de consommation finale dont administrations publiques", "Formation brute de capital fixe", "Formation brute de capital fixe dont sociétés et entreprises individuelles non financières",
+                          "Formation brute de capital fixe dont administrations publiques", " Formation brute de capital fixe dont ménages hors entrepreneurs individuels", "Exportations de biens et de services",
+                          "Demande intérieure hors stocks"], 1]
+            },
         ]
     },
     {
