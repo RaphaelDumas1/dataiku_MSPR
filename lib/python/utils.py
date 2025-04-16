2840c247a802fc93e39575920c2a7148526a2146
admin <test@gmail.com>
2025-04-16 18:29:40 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -268,9 +268,7 @@ def add_columns(df, col1, col2, result_column):
     return df
 
 def rename_columns(df, columns_dict):
-    df.rename(columns=columns_dict)
-    print("mmm", df.columns)
-    return df
+    return df.rename(columns=columns_dict)
 
 def process_pib(df):
     rows_to_prefix = [9, 10, 13, 14, 15]
