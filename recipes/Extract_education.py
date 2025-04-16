fbc008f81337d1963560a79986ddc8739bdc5908
admin <test@gmail.com>
2025-04-16 09:56:14 +0000

    Saved recipe: MSPR.Extract_education

--- recipes/Extract_education.py
+++ recipes/Extract_education.py
@@ -20,7 +20,31 @@ datasets = [
     {
         "name": "annuaire_des_ecoles_en_france",
         "functions": [
-            
+            {
+                "name" : delete_where_not_equal,
+                "args" : ["code_departement", 35.0]
+            },
+            {
+                "name" : delete_columns_by_name,
+                "args" : [['code_departement', 'code_region', 'libelle_departement', 'libelle_region']]
+            },
+            {
+                "name" : process_annuaire,
+                "args" : []
+            },
+            {
+                 "name" : columns_to_string,
+                 "args" : [["code_postal", "code_commune", "code_academie", "telephone",
+                           "siren_siret", "coordx_origine", "coordy_origine", "rpi_disperse", "code_type_contrat_prive", "code_bassin_formation",
+                           "code_nature", "code_zone_animation_pedagogique"]]
+            },
+            {
+                 "name" : columns_to_int,
+                 "args" : [["ecole_maternelle", "ecole_elementaire", "voie_generale", "voie_technologique", "voie_professionnelle",
+                           "restauration", "hebergement", "ulis", "apprentissage", "segpa", "section_arts", "section_cinema", "section_theatre",
+                           "section_sport", "section_internationale", "section_europeenne", "lycee_agricole", "lycee_militaire", "lycee_des_metiers",
+                           "post_bac", "greta", "nombre_d_eleves", "multi_uai", "rpi_concentre"]]
+            }
         ]
     },
 ]
