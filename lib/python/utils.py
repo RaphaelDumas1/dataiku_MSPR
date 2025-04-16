1ac9f1763e56e890a8fb5d6c40e2855f1297b4ff
admin <test@gmail.com>
2025-04-16 14:15:57 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -266,10 +266,10 @@ def add_columns(df, col1, col2, result_column):
 
     return df
 
-def rename_column(df, old_name, new_name):
+def rename_columns(df, columns_dict):
     if old_name not in df.columns:
         raise ValueError(f"La colonne '{old_name}' n'existe pas dans le DataFrame.")
-    return df.rename(columns={old_name: new_name})
+    return df.rename(columns=columns_dict)
 
 def process_pib(df):
     rows_to_prefix = [9, 10, 13, 14, 15]
