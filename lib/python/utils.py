6894315d13173ea416a34bc7e3fc885f59f57baa
admin <test@gmail.com>
2025-04-16 11:45:23 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -331,10 +331,8 @@ def fill_empty_values(df, columns_defaults):
 def fill_empty_values_with_mean(df, columns):
     for col in columns:
         is_column_in_dataframe(df, col)
-        df[col] = df[col].fillna("").apply(lambda x: x if str(x).strip() else default)
-    return df
-    is_column_in_dataframe(df, col)
-    return df[column].fillna(df[column].mean())
+        return df[column].fillna(df[column].mean())
+    
         
 def execute_instruction_on_dataframe(df, title, instruction):
     engine = create_engine('postgresql://postgres:test@host.docker.internal:5432/MSPR')
