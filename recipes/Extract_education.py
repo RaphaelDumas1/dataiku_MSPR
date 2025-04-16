502ec8d8870be429cf2a2d0a790c754cbd51d3e2
admin <test@gmail.com>
2025-04-16 10:00:03 +0000

    Saved recipe: MSPR.Extract_education

--- recipes/Extract_education.py
+++ recipes/Extract_education.py
@@ -1,2 +1,2 @@
-from utils import create_datasets_from_file_sheets, columns_to_int, process_annuaire, columns_to_string, delete_where_not_equal, delete_columns_by_name
+from utils import create_datasets_from_file_sheets, columns_to_int, process_annuaire, columns_to_string, delete_where_not_equal
 
