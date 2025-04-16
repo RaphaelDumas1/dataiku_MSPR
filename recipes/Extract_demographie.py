bbf14617e73434774b6c4695fd92ade4c0ad23ed
admin <test@gmail.com>
2025-04-16 20:36:22 +0000

    Saved recipe: MSPR.Extract_demographie

--- recipes/Extract_demographie.py
+++ recipes/Extract_demographie.py
@@ -15,7 +15,7 @@ datasets = [
         "functions": [
             {
                  "name" : extract_and_concat_to_original,
-                 "args" : [(802, 1604), (1606, 2804)]
+                 "args" : [(801, 1603), (1604, 2802)]
             },
             {
                  "name" : delete_columns_by_name,
