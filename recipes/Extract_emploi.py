837a3b55e894e3e6f921e70c60cb18da064e7e83
admin <test@gmail.com>
2025-04-17 22:35:12 +0000

    Saved recipe: MSPR.Extract_emploi

--- recipes/Extract_emploi.py
+++ recipes/Extract_emploi.py
@@ -1,4 +1,4 @@
-from utils import create_datasets_from_file_sheets, process_category_metier, pivot, columns_to_int, process_evolution_trimestrielle_emploi, rename_columns, delete_rows_by_index, make_unique, set_row_as_headers  
+from utils import create_datasets_from_file_sheets, process_category_metier, pivot, columns_to_int, process_evolution_trimestrielle_emploi, rename_columns, delete_rows_by_index, make_unique, set_row_as_headers, complete_with_inteprolate  
 
 datasets = [
     {
