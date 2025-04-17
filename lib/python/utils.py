db713d026583b8eb9392595fab7b147cc43fafdf
admin <test@gmail.com>
2025-04-17 21:26:02 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -319,7 +319,7 @@ def execute_instruction_on_dataframe(df, title, instruction):
         year_window = set(range(2006, 2024))
         df_years = set(df['année'])
         missing_years = sorted(year_window - df_year)
-        
+        print("Années manquantes :", missing_years)
         
         
     # Exportation vers PostgreSQL
