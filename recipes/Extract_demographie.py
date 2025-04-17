00448ad6f8f8d546f5a55821f2711654050f553d
admin <test@gmail.com>
2025-04-17 22:50:50 +0000

    Saved recipe: MSPR.Extract_demographie

--- recipes/Extract_demographie.py
+++ recipes/Extract_demographie.py
@@ -8,6 +8,10 @@ datasets = [
                  "name" : columns_to_int,
                  "args" : []
             },
+            {
+                 "name" : complete_with_inteprolate,
+                 "args" : []
+            },
         ]
     },
     # {
