48f6f997a4183e6936cf2d60cefd14cc0276d161
admin <test@gmail.com>
2025-04-19 17:41:53 +0000

    Saved scenario MSPR.bdd (id: BDD)

--- scenarios/BDD.py
+++ scenarios/BDD.py
@@ -261,7 +261,7 @@ with engine.connect() as conn:
                             "fait_demographqiue_id" : id_fait_demographique,
                         }
                         queries.append(buildInsertQuery(row, "fait_demographique_has_dim_age", {row_unique[col] : "total"}, col_mapping, False))
-                executeQueries(conn, queries, "dim_age")
+                executeQueries(conn, queries, "fait_demographique_has_dim_age")
             
             
                 
