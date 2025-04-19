6014787392fd5afc84900d0f11ae144de6c69c04
admin <test@gmail.com>
2025-04-19 16:15:28 +0000

    Saved scenario MSPR.bdd (id: BDD)

--- scenarios/BDD.py
+++ scenarios/BDD.py
@@ -84,9 +84,7 @@ tables = [
             "4 enfants ou plus" : "quatre_et_plus"
         },
         "id": None,
-        "add": [
-            {"name": "dim_annee_id", "value": "dim_annee"}  # Ajouter une colonne avec l'ID de dim_annee
-        ]  # Liste vide pour dim_annee car il n'y a pas de colonnes supplémentaires
+        "add": {"dim_annee_id" :"dim_annee"}
     },
     {
         "name": "fait_demographique",
