d5dcd0fb24d541d9924a2af700e54c5cf0217605
admin <test@gmail.com>
2025-04-19 16:14:17 +0000

    Saved scenario MSPR.bdd (id: BDD)

--- scenarios/BDD.py
+++ scenarios/BDD.py
@@ -146,9 +146,7 @@ tables = [
             "taux" : "taux_chomage"
         },
         "id": None,
-        "add": [
-            {"dim_annee_id" : "dim_annee"}  # Ajouter une colonne avec l'ID de dim_annee
-        ]  # Liste vide pour dim_annee car il n'y a pas de colonnes supplémentaires
+        "add": {"dim_annee_id" : "dim_annee"}
     },
     {
         "name": "fait_logement",
