680705a8eb57df29b50d293c096d22be113210d8
admin <test@gmail.com>
2025-04-22 19:32:37 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -79,8 +79,6 @@ def create_datasets_from_file_sheets(file_name, instructions):
         df = create_dataframe_from_sheet(sheet)
         df = execute_instructions_on_dataframe(df, title, instructions[title])
         
-        # General post treatment
-        
         df.columns = [unidecode(col).lower() for col in df.columns]
         
         # Remove years before 2006
