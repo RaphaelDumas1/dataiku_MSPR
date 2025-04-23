256bd1e2c1c0fb6225bf4fda681b9a015260470a
admin <test@gmail.com>
2025-04-23 17:47:08 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -264,7 +264,7 @@ def fill_empty_values(df, values_dict):
 
 
 
-# Used to fill empty values of numericals columns from a list with the mean of the others values of the column
+# Used to fill empty values of numericals columns from a list with the mean of the others values of the columns
 def fill_empty_values_with_mean(df, columns):
     check_columns_exist(df, columns)
     
