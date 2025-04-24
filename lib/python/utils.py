aa0cbd260132247c4fadcd303cb5fe530910ddcd
admin <test@gmail.com>
2025-04-24 12:55:13 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -1,3 +1,13 @@
+def get_float_precision(df, float_cols):
+    float_precision = {}
+    for col in float_cols:
+        non_null = df[col].dropna()
+        if not non_null.empty:
+            float_precision[col] = non_null.map(lambda x: len(str(x).split(".")[1]) if "." in str(x) else 0).max()
+        else:
+            float_precision[col] = 1
+    return float_precision
+
 def pivot(df, first_column_name):
     # Pivot
     df.set_index(df.columns[0], inplace=True)
@@ -83,15 +93,7 @@ def add_columns(df, col1, col2, result_column):
 
     return df
 
-def get_float_precision(df, float_cols):
-    float_precision = {}
-    for col in float_cols:
-        non_null = df[col].dropna()
-        if not non_null.empty:
-            float_precision[col] = non_null.map(lambda x: len(str(x).split(".")[1]) if "." in str(x) else 0).max()
-        else:
-            float_precision[col] = 1
-    return float_precision
+
 
 
 
