722392044492941b953d25e1acc3c744d8bcca8a
admin <test@gmail.com>
2025-04-24 12:59:28 +0000

    Edited Library file "python/delete.py"

--- lib/python/delete.py
+++ lib/python/delete.py
@@ -33,8 +33,3 @@ def delete_rows_by_index(df, indexes, max_index=None):
         df = df[df.index <= max_index]
         
     return df.drop(index=[i for i in indexes if i < len(df)], errors='ignore').reset_index(drop=True)
-
-# Used to delete row(s) in a dataframe where column values lower than min_range parameter and higher than max_range parameter
-def delete_rows_not_in_range(df, column, min_range, max_range):
-    check_columns_exist(df, [column])
-    return df[(df[column] >= min_range) & (df[column] <= max_range)]
