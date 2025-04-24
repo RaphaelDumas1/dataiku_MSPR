6859e9682a5eea8f9bd5fe27fabea8c7281cd2de
admin <test@gmail.com>
2025-04-24 13:23:44 +0000

    Saved recipe: MSPR.Extract_sante

--- recipes/Extract_sante.py
+++ recipes/Extract_sante.py
@@ -1,4 +1,5 @@
-from utils import create_datasets_from_file_sheets, delete_rows_where_equal, columns_to_int, columns_to_float, rename_columns, complete_with_inteprolate
+from global import create_datasets_from_file_sheets, 
+delete_rows_where_equal, columns_to_int, columns_to_float, rename_columns, complete_with_inteprolate
 
 datasets = {
     "Esperance_de_vie" : [
