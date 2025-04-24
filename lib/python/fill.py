424d426a41dca0301f69976180d40640ffd6242f
admin <test@gmail.com>
2025-04-24 13:01:00 +0000

    Edited Library file "python/fill.py"

--- lib/python/fill.py
+++ lib/python/fill.py
@@ -24,8 +24,8 @@ def fill_empty_values_with_mean(df, columns):
 
 # Used to fill empty values of numericals columns with interpolation
 def fill_with_interpolation(df, columns_to_exlude=[]):
-    int_cols = df.select_dtypes(include='int').columns.drop('année', errors='ignore')
-    float_cols = df.select_dtypes(include='float').columns.drop('année', errors='ignore')
+    int_columns = df.select_dtypes(include='int').columns.drop(columns_to_exlude, errors='ignore')
+    float_columns = df.select_dtypes(include='float').columns.drop(columns_to_exlude, errors='ignore')
     float_precision = get_float_precision(df, float_cols)
     
     num_cols = df.select_dtypes(include='number').columns.drop(columns_to_exlude, errors='ignore')
