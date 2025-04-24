626d9e6dc2bf948a5eb2e8008b88b70cccedd7fa
admin <test@gmail.com>
2025-04-24 12:38:25 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -406,6 +406,15 @@ def add_columns(df, col1, col2, result_column):
 
     return df
 
+def get_float_precision(df, float_cols):
+    float_precision = {}
+    for col in float_cols:
+        non_null = df[col].dropna()
+        if not non_null.empty:
+            float_precision[col] = non_null.map(lambda x: len(str(x).split(".")[1]) if "." in str(x) else 0).max()
+        else:
+            float_precision[col] = 1
+    return float_precision
 
 
 #
