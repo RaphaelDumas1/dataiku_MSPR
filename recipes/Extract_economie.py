962e97676325c1672d8b29639415ee8db9ec4294
admin <test@gmail.com>
2025-04-24 14:55:19 +0000

    Saved recipe: MSPR.Extract_economie

--- recipes/Extract_economie.py
+++ recipes/Extract_economie.py
@@ -6,49 +6,52 @@ from delete import delete_rows_by_index
 def pib_column(columns):
     return columns.astype(str).str.strip().str.replace(" \(r\)", "", regex=True)
 
-datasets = [
+datasets = {
+    "Pib" : [
+        {
+            "name" : set_row_as_headers,
+            "args" : [1, pib_column]
+        },
+        {
+            "name" : delete_rows_by_index,
+            "args" : [[0, 1, 2, 3, 6, 8, 12], 17]
+        },
+        {
+            "name" : pivot,
+            "args" : ["Année"]
+        },
+        {
+            "name" : rename_columns,
+            "args" : [{
+                "ménages": "Dépense de consommation finale dont ménages",
+                "administrations publiques": "Dépense de consommation finale dont administrations publiques",
+                "sociétés et entreprises individuelles non financières": "Formation brute de capital fixe dont sociétés et entreprises individuelles non financières",
+                "administrations publiques_1": "Formation brute de capital fixe dont administrations publiques",
+                "ménages hors entrepreneurs individuels": "Formation brute de capital fixe dont ménages hors entrepreneurs individuels"
+            }]
+        },
+        {
+            "name" : columns_to_int,
+            "args" : [["Année"]]
+        },
+        {
+            "name" : columns_to_float,
+            "args" : [[" Produit intérieur brut (PIB)", "Importations de biens et de services", "Dépense de consommation finale", "Dépense de consommation finale dont ménages",
+                      "Dépense de consommation finale dont administrations publiques", "Formation brute de capital fixe", "Formation brute de capital fixe dont sociétés et entreprises individuelles non financières",
+                      "Formation brute de capital fixe dont administrations publiques", " Formation brute de capital fixe dont ménages hors entrepreneurs individuels", "Exportations de biens et de services",
+                      "Demande intérieure hors stocks"], 1]
+        },
+        {
+            "name" : complete_with_inteprolate,
+            "args" : []
+        },
+
+    ],
+    
+}
     {
-        "name": "Pib",
-        "functions": [
-            {
-                "name" : set_row_as_headers,
-                "args" : [1, pib_column]
-            },
-            {
-                "name" : delete_rows_by_index,
-                "args" : [[0, 1, 2, 3, 6, 8, 12], 17]
-            },
-            {
-                "name" : pivot,
-                "args" : ["Année"]
-            },
-            {
-                "name" : rename_columns,
-                "args" : [{
-                    "ménages": "Dépense de consommation finale dont ménages",
-                    "administrations publiques": "Dépense de consommation finale dont administrations publiques",
-                    "sociétés et entreprises individuelles non financières": "Formation brute de capital fixe dont sociétés et entreprises individuelles non financières",
-                    "administrations publiques_1": "Formation brute de capital fixe dont administrations publiques",
-                    "ménages hors entrepreneurs individuels": "Formation brute de capital fixe dont ménages hors entrepreneurs individuels"
-                }]
-            },
-            {
-                "name" : columns_to_int,
-                "args" : [["Année"]]
-            },
-            {
-                "name" : columns_to_float,
-                "args" : [[" Produit intérieur brut (PIB)", "Importations de biens et de services", "Dépense de consommation finale", "Dépense de consommation finale dont ménages",
-                          "Dépense de consommation finale dont administrations publiques", "Formation brute de capital fixe", "Formation brute de capital fixe dont sociétés et entreprises individuelles non financières",
-                          "Formation brute de capital fixe dont administrations publiques", " Formation brute de capital fixe dont ménages hors entrepreneurs individuels", "Exportations de biens et de services",
-                          "Demande intérieure hors stocks"], 1]
-            },
-            {
-                "name" : complete_with_inteprolate,
-                "args" : []
-            },
-            
-        ]
+        "name": ,
+        "functions": 
     },
     {
         "name": "Inflation",
