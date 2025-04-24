cc118e8d0971859760276f9ebfb9cf17e656aecb
admin <test@gmail.com>
2025-04-24 14:00:21 +0000

    Saved recipe: MSPR.Extract_securite

--- recipes/Extract_securite.py
+++ recipes/Extract_securite.py
@@ -1,4 +1,6 @@
-from utils import create_datasets_from_file_sheets, columns_to_int, pivot, complete_with_inteprolate
+from general import create_datasets_from_file_sheets
+from convert import convert_columns 
+from other import pivot
 
 
 
@@ -9,18 +11,29 @@ instructions = {
              "args" : ["Année"]
         },
         {
-             "name" : columns_to_int,
-             "args" : []
+             "name" : convert_columns,
+             "args" : [{
+                 "Année" : 'int',
+                 "personnes prises en charge" : 'int',
+                 "mesures en cours" : 'int',
+                 "sursis1" : 'int',
+                 "travail d'interet general (tig)2" : 'int',
+                 "liberations conditionnelles3" : 'int',
+                 "autres mesures : 'int"
+             }]
         }, 
-        {
-             "name" : complete_with_inteprolate,
-             "args" : []
-        },
     ],
     "Delinquance" : [
         {
-             "name" : columns_to_int,
-             "args" : [{"Année", "nombre", "insee_pop", "insee_pop_millesime", "insee_log", "insee_log_millesime"}]
+             "name" : convert_columns,
+             "args" : [{
+                 "Année" : 'int', 
+                 "nombre": 'int',
+                 "insee_pop": 'int',
+                 "insee_pop_millesime": 'int',
+                 "insee_log": 'int',
+                 "insee_log_millesime": 'int'
+             }]
         }
     ]
 }
