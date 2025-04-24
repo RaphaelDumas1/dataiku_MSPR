781705b4e14ce0880f1423926e45bdb8589968dd
admin <test@gmail.com>
2025-04-24 13:05:05 +0000

    Edited multiple Library files

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -9,3 +9,24 @@ def get_float_precision(df, float_cols):
     return float_precision
 
 
+
+
+# Used to make each element of a list unique by adding a number at the end of it to prevent duplicates   
+def make_list_values_unique(list):
+    seen = {}
+    result = []
+    for values in list:
+        values = str(values).strip()
+        if values not in seen:
+            seen[values] = 1
+            result.append(values)
+        else:
+            count = seen[values]
+            new_values = f"{values}_{count}"
+            while new_values in seen:
+                count += 1
+                new_h = f"{values}_{count}"
+            seen[values] = count + 1
+            seen[new_values] = 1
+            result.append(new_values)
+    return result 
