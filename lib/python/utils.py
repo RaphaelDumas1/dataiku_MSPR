630dcd76f33164ed3c6b97b28ee254c36b9b7352
admin <test@gmail.com>
2025-04-24 12:57:16 +0000

    Edited Library file "python/utils.py"

--- lib/python/utils.py
+++ lib/python/utils.py
@@ -9,25 +9,6 @@ def get_float_precision(df, float_cols):
     return float_precision
 
 
-
-def copy_years_range(df):
-    # Add row for each year in range
-    rows = []
-
-    for index, row in df.iterrows():
-        year_range = str(row['Année'])
-        start_year, end_year = map(int, year_range.split('-'))
-
-        if(index == (len(df) - 1)):
-            end_year = end_year + 1
-
-        for year in range(start_year, end_year):
-            new_row = row.copy()
-            new_row['Année'] = str(year)
-            rows.append(new_row)
-    
-    return pd.DataFrame(rows)
-
 def process_category_metier(df):
     # Remove headers rows
     df = df.drop(index=1)
